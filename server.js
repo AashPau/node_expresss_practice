d11fdb72ceebc948abc450769bfd5b8975098468
Aash <31497659+AashPau@users.noreply.github.com>
2024-04-15 13:38:46 +1000

    aaded functionalities to login and register page

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 import express from "express";
 
+import fs from "fs";
+
 import path from "path";
 const app = express();
 
 const __dirname = path.resolve();
 
+const fn = "userList.csv";
+
 app.use(express.urlencoded());
 
 app.post("/register", (req, res) => {
-	console.log(req.body);
-	res.send("Data received");
+	const { email, password } = req.body;
+	const str = email + "," + password + "\n";
+
+	fs.appendFile(fn, str, (error) => {
+		console.log(error);
+	});
+
+	res.redirect("/login");
 });
 
 app.get("/register", (req, res) => {
@@ -20,6 +30,26 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/public/index.html");
 });
 
+app.get("/login", (req, res) => {
+	res.sendFile(__dirname + "/public/login.html");
+});
+
+app.post("/login", (req, res) => {
+	const { email, password } = req.body;
+
+	const loginDetails = email + "," + password;
+
+	const str = email + "," + password + "\n";
+	//read the file data
+	fs.readFile(fn, (error, data) => {
+		const str = data.toString();
+
+		if (str.includes(loginDetails)) {
+			return res.send("<h1> login successful<h1>");
+		}
+		res.send(`<h1 style="color:red">Login failed</h1>`);
+	});
+});
 app.listen(8000, (error) => {
 	error
 		? console.log(error)
